dafe50bdfb50bf492d11188c1f7ff7bf1cff9b82
Ning0112 <B10708012@gapps.knu.edu.tw>
2020-03-05 18:12:54 +0800

    1_train_resnet50.py
    訓練檔

---
+++ 1_train_resnet50.py
@@ -0,0 +1,94 @@
+#https://blog.gtwang.org/programming/keras-resnet-50-pre-trained-model-build-dogs-cats-image-classification-system/
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Flatten, Dense, Dropout
+from tensorflow.python.keras.applications.resnet50 import ResNet50
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+# 資料路徑
+DATASET_PATH  = 'sample'
+
+# 影像大小
+IMAGE_SIZE = (224, 224)
+
+# 影像類別數
+NUM_CLASSES = 2
+
+# 若 GPU 記憶體不足，可調降 batch size 或凍結更多層網路
+BATCH_SIZE = 8
+
+# 凍結網路層數
+FREEZE_LAYERS = 2
+
+# Epoch 數 20
+NUM_EPOCHS = 1
+
+# 模型輸出儲存的檔案
+WEIGHTS_FINAL = 'model-resnet50-final.h5'
+
+# 透過 data augmentation 產生訓練與驗證用的影像資料
+train_datagen = ImageDataGenerator(rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   channel_shift_range=10,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
+train_batches = train_datagen.flow_from_directory(DATASET_PATH + '/train',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=True,
+                                                  batch_size=BATCH_SIZE)
+
+valid_datagen = ImageDataGenerator()
+valid_batches = valid_datagen.flow_from_directory(DATASET_PATH + '/valid',
+                                                  target_size=IMAGE_SIZE,
+                                                  interpolation='bicubic',
+                                                  class_mode='categorical',
+                                                  shuffle=False,
+                                                  batch_size=BATCH_SIZE)
+
+# 輸出各類別的索引值
+for cls, idx in train_batches.class_indices.items():
+    print('Class #{} = {}'.format(idx, cls))
+
+# 以訓練好的 ResNet50 為基礎來建立模型，
+# 捨棄 ResNet50 頂層的 fully connected layers
+net = ResNet50(include_top=False, weights='imagenet', input_tensor=None,
+               input_shape=(IMAGE_SIZE[0],IMAGE_SIZE[1],3))
+x = net.output
+x = Flatten()(x)
+
+# 增加 DropOut layer
+x = Dropout(0.5)(x)
+
+# 增加 Dense layer，以 softmax 產生個類別的機率值
+output_layer = Dense(NUM_CLASSES, activation='softmax', name='softmax')(x)
+
+# 設定凍結與要進行訓練的網路層
+net_final = Model(inputs=net.input, outputs=output_layer)
+for layer in net_final.layers[:FREEZE_LAYERS]:
+    layer.trainable = False
+for layer in net_final.layers[FREEZE_LAYERS:]:
+    layer.trainable = True
+
+# 使用 Adam optimizer，以較低的 learning rate 進行 fine-tuning
+net_final.compile(optimizer=Adam(lr=1e-5),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+# 輸出整個網路結構
+print(net_final.summary())
+
+# 訓練模型
+net_final.fit_generator(train_batches,
+                        steps_per_epoch = train_batches.samples // BATCH_SIZE,
+                        validation_data = valid_batches,
+                        validation_steps = valid_batches.samples // BATCH_SIZE,
+                        epochs = NUM_EPOCHS)
+
+# 儲存訓練好的模型
+net_final.save(WEIGHTS_FINAL)
